010972f370d7da1b4905469f5eb9ebb559026a50
princeyadav0136 <princeyadav0136@gmail.com>
2024-02-10 09:25:37 +0530

    Add space

--- src/components/Body.js
+++ src/components/Body.js
@@ -7,6 +7,7 @@ import RestaurantCard from "./RestaurantCard";
 const Body = () => {
 
     const [restaurantList, setRestaurantList] = useState(resList);
+    
 
     return (
         <div className="body">
